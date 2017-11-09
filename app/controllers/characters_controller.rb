84facd83d7ae90e4d073b31495ed35afe676719d
Ryan <rblument@uncc.edu>
2017-11-09 19:36:56 +0000

    Complete create and show

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,6 +2,16 @@ class CharactersController < ApplicationController
     def new
     end
     def create
-        render plain: params[:character].inspect
+        @character = Character.new(article_params)
+        @character.save
+        redirect_to @character
+    end
+    def show
+        @character = Character.find(params[:id])
     end
 end
+
+private
+    def article_params
+        params.require(:character).permit(:charname, :level, :charclass, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :race)
+    end
