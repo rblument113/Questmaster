692415d9550eb6c067c6cc96ecb96606db08e58c
Ryan <rblument@uncc.edu>
2017-11-13 22:21:02 +0000

    Added remaining input validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ApplicationRecord
     validates :charname, presence: true, length: { minimum: 2 }
-    
+    validates :level, presence: true, numericality: { greater_than: 0, less_than: 21 }
+    validates :strength, presence: true, numericality: { greater_than: 0, less_than: 21 }
+    validates :dexterity, presence: true, numericality: { greater_than: 0, less_than: 21 }
+    validates :wisdom, presence: true, numericality: { greater_than: 0, less_than: 21 }
+    validates :constitution, presence: true, numericality: { greater_than: 0, less_than: 21 }
+    validates :intelligence, presence: true, numericality: { greater_than: 0, less_than: 21 }
+    validates :charisma, presence: true, numericality: { greater_than: 0, less_than: 21 }
 end
