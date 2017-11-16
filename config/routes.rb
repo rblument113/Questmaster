3725f2cf555c5e62e0196b289c61daad04904d4a
Ryan <rblument@uncc.edu>
2017-11-16 00:48:21 +0000

    Add comments routes after completely missing an extraneous end statement (sorry)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :characters do
-    resources :character_feats_maps
-  end
-  resources :feats do
-    resources :character_feats_maps
+    resources :feats
   end
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
