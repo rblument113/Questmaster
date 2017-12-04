cd13da463c314a416954a6f132af7f5862dfc794
Ryan <rblument@uncc.edu>
2017-12-04 20:59:16 +0000

    Add articles controller

---
+++ app/controllers/spells_controller.rb
@@ -0,0 +1,12 @@
+class SpellsController < ApplicationController
+    def create
+       @character = Carachter.find(params[:character_id])
+       @spell = @character.spells.create(spell_params)
+       redirect_to character_path(@character)
+    end
+    
+    private
+        def spell_params
+           params.require(:spell).permit(:name, :level, :castingtime, :range, :components, :duration)
+        end
+end
