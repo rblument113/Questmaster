4a573814d0fcea5c9be78a41772e2f088e72332c
Ryan <rblument@uncc.edu>
2017-12-07 23:52:53 +0000

    Add missing delete fuctionality to associated models

--- app/controllers/feats_controller.rb
+++ app/controllers/feats_controller.rb
@@ -4,6 +4,12 @@ class FeatsController < ApplicationController
         @feat = @character.feats.create(feat_params)
         redirect_to character_path(@character)
     end
+    def destroy
+       @character = Character.find(params[:character_id])
+       @feat = @character.feats.find(params[:id])
+       @feat.destroy
+       redirect_to character_path(@character)
+    end
     private
         def feat_params
             params.require(:feat).permit(:featname, :level)
--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -4,7 +4,12 @@ class SpellsController < ApplicationController
        @spell = @character.spells.create(spell_params)
        redirect_to character_path(@character)
     end
-    
+    def destroy
+       @character = Character.find(params[:character_id])
+       @spell = @character.spells.find(params[:id])
+       @spell.destroy
+       redirect_to character_path(@character)
+    end
     private
         def spell_params
            params.require(:spell).permit(:name, :level, :castingtime, :range, :components, :duration)
